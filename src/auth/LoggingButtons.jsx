306aaf4c630e1554d1d81dad18fcf20bbb48b874
“Mariana <marysiq@hotmail.com>
2024-11-20 14:37:27 -0800

    loggingButtons done

--- src/auth/LoggingButtons.jsx
+++ src/auth/LoggingButtons.jsx
@@ -1,20 +1,19 @@
-import 
+import { useAuth0 } from '@auth0/auth0-react';
 
 /**
  * TODO: Ticket 3:
  * Implement authentication and logging functionality using Auth0
  */
 export const LoggingButtons = () => {
-  // TODO: Replace these with Auth0 functionality
-  const isAuthenticated = false;
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   const buttonText = isAuthenticated ? 'Log Out' : 'Log In';
 
   const handleLogging = () => {
     if (isAuthenticated) {
-      // TODO: Add Logout functionality here:
+      logout({ returnTo: window.location.origin });
     } else {
-      // TODO: Add Redirect functionality here:
+      loginWithRedirect();
     }
   };
 
