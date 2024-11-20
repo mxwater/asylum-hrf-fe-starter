12e5d7cb88f884c8d38b3cd439a89c577348cc8e
“Mariana <marysiq@hotmail.com>
2024-11-20 13:51:32 -0800

    installed auth0 + working on main +  app working

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App.jsx';
 import { ProvideAppContext } from './context/AppContext.jsx';
+import { Auth0Provider } from '@auth0/auth0-react'; 
 
-const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
-const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
+const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN; 
+const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID; 
 
 /**
  * TODO: Ticket 3:
@@ -13,8 +14,17 @@ const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
  * - Add your credentials from Auth0 to a .env file (AUTH_DOMAIN, AUTH_CLIENT_ID)
  * - Set the domain, clientId, and authorizationParams
  */
+
 createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+    domain={AUTH_DOMAIN}
+    clientId={AUTH_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: window.location.origin, 
+    }}
+  >
     <ProvideAppContext>
       <App />
     </ProvideAppContext>
+  </Auth0Provider>
 );
